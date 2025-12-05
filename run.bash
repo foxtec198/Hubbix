d91d70d3ae2264cad4820af972360d7af5f9dbff
Guilherme Breve <foxtec198@gmail.com>
2025-12-05 13:40:02 -0300

    fix: Structure from wsgi code

--- run.bash
+++
@@ -1,6 +0,0 @@
-/home/guibs/Documentos/Hubbix/venv/bin/python3 -m gunicorn wsgi:app \
-    --bind 0.0.0.0:9560 \
-    --timeout 120 \
-    --log-level info \
-    --access-logfile - \
-    --error-logfile -
