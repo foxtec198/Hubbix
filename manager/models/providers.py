cb547ada9097faf179aac848652530203f7b4979
Guilherme Breve <foxtec198@gmail.com>
2025-12-15 18:00:14 -0300

    Feat: Providers add

--- manager/models/providers.py
+++ manager/models/providers.py
@@ -2,10 +2,29 @@ from utils.db import db
 from general.models.base_model import BaseModel
 
 class Provider(BaseModel):
-    __bind_key__ = "manager"
-    __tablename__ = "fornecedores"
+    __bind_key__ = "manager" # banco de dados
+    __tablename__ = "fornecedores" # Tabela
 
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String)
-    cr = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    nome = db.Column(db.String, nullable=False) # Nome do Forn.
+    telefone = db.Column(db.String, nullable=True) # Telefone
+    cr = db.Column(db.String, nullable=False) # "ID" da Loja
+    grupodecliente = db.Column(db.String, nullable=False) # "ID" do Grupo de Cliente
+
+    @classmethod
+    def get_provider(provider, cr, id) -> dict:
+        provider.query.filter( # Obtem um fornecedor em especifico de acordo com id e o cr
+            provider.cr == cr,
+            provider.id == id
+        ).first()
+        return provider
+    
+    @classmethod
+    def _search_by_cr(provider, cr) -> list:
+        # Obtem os fornecedores de acordo com a loja (CR)
+        providers = provider.query.filter(provider.cr == cr).all()
+
+        # Retorna uma lista de fornecedores por loja 
+        return [prov.to_dict() for prov in providers]
+
     
--- manager/services/providers_service.py
+++ manager/services/providers_service.py
@@ -3,28 +3,66 @@ from manager.models.providers import Provider
 from utils.safe_route import require_cr, check_connection
 from utils.db import db
 from flask import jsonify
+from utils.check_field import check_field
 
 class ProviderService:
     @check_connection
     @require_cr
-    def get(self, bd:MultiDict, hd:Headers, cr=None) -> tuple:
+    def get(self, bd:MultiDict, cr=None) -> tuple:
         id = bd.get("id")
-        if id:
-            provider = Provider.query.filter_by(id=id, cr=cr).first()
-            if provider: return jsonify(provider.to_dict()), 200
-            else: return jsonify("Fornecedor não encontrado"), 404
-        return jsonify([provider.to_dict() for provider in Provider.query.filter_by(cr=cr).all()]), 200
-        
+        if id: # Confirma se foi passado o id no argumentos
+            provider = Provider.get_provider(cr, id)
+            if provider: # retorna o fornecedor caso exista, se nao retorna NAO ENCONTRADO - 404
+                return jsonify(provider.to_dict()), 200 
+            return jsonify("Fornecedor não encontrado"), 404
+        # Caso não seja passado o id retorna a lista por CR
+        return jsonify(Provider._search_by_cr(cr)), 200
 
     @check_connection
     @require_cr
     def create(self, bd:MultiDict, hd:Headers, cr=None) -> tuple:
-        ...
+        # =============== Dados do Fornecedor e da Loja (CR, GC)
+        nome = bd.get("nome") # Nome do Forn.
+        telefone = bd.get("telefone") # Telefone do Forn.
+        gc = hd.get("gc") # Grupo de Cliente - Passado no Headers
+        
+        # =============== Conferencias e retorno
+        # Confirma os campos obrigatórios 
+        # Apenas um campo obrigatório, mas caso necessite de mais, é só adiconar a função
+        ok, error = check_field(nome=nome)
+
+        if ok: # Caso todos os dados obrigatórios estiverem presentes
+            provider = Provider( # Cria o provider e seta seus dados
+                nome = nome,
+                telefone = telefone,
+                cr = cr,
+                grupodecliente = gc
+            )
+            db.session.add(provider) # Adiciona ao Banco de Dados
+            db.session.commit() # Salva as alterações
+            # Retorna Sucesso e o ID
+            return jsonify({"msg": "Fornecedor criado", "id": provider.id}), 201
+        return jsonify(error), 400 # Retorna erro de requisição
+
 
     @check_connection
     @require_cr
     def update(self, bd:MultiDict, hd:Headers, cr=None) -> tuple:
-        ...
+        # ================= Dados a serem atualizados!!!
+        id = bd.get("id") # ID do Fornecedor
+        nome = bd.get("nome") # Nome do Fornecedor
+        telefone = bd.get("telefone") # Telefone do Fornecedor
+
+        # ================= Conferencias e Retornos
+        if id: # ID Obrigatório
+            provider = Provider.get_provider(cr, id) # Obtem o fornecedor
+            if provider: # Caso exista da a continuidade
+                if nome: provider.nome = nome # Atualiza o nome caso seja declarado
+                if telefone: provider.telefone = telefone # Atualiza o telefone caso seja declarado
+                db.session.commit() # Salva as alterações
+                return jsonify("Fornecedor atualizado"), 200 # Retorna Sucesso
+            return jsonify("Fornecedor nao encontrado"), 404  # Retorna, NAO ENCONTRADO - 404
+        return jsonify("ID Obrigatório"), 400  # Retorna erro de requisição, BAD REQUEST - 400
 
     @check_connection
     @require_cr
