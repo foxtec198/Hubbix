6f4c7c09289f910ee8c9f843e153bd0fd0e942f2
Guilherme Breve <foxtec198@gmail.com>
2026-04-06 16:12:30 -0300

    Fix route parameter type from 'str' to 'string' in email_routes.py

--- general/routes/email_routes.py
+++ general/routes/email_routes.py
@@ -4,7 +4,7 @@ from general.services.email_service import EmailService
 email_bp = Blueprint("Emails", __name__)
 email_service = EmailService()
 
-@email_bp.route("/<str:email>", methods=["POST"]) # Envia um email
+@email_bp.route("/<string:email>", methods=["POST"]) # Envia um email
 def main(email):
     match rq.method: 
         case "POST": return email_service.send_mail(email)
