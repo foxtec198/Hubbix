e59c8b2ead9bfae9f223dbd863630f5dcc4cc2ac
Guilherme Breve <foxtec198@gmail.com>
2026-04-06 16:12:09 -0300

    Enhance startup logging and improve database initialization messages

--- app.py
+++ app.py
@@ -6,12 +6,19 @@ from os import getenv
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
+# Printa inicialização da API
+print("="*20)
+print("Iniciando Hubbix API")
+print("="*20)
+
 load_dotenv() # Carrega as variaveis de ambiente
+print("[✅] - Variaveis de ambiente carregadas com sucesso")
+
+print("Configurando API...")
 app = Flask(__name__) # Cria a instacia do APP
 CORS(app) # CORS Policy 
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode="gevent") # Cria o SocketIO
 app.config["SECRET_KEY"] = getenv("KEY") # Seta SUPERSECRET key, lol.
-
 app.config["SQLALCHEMY_BINDS"] = {  # Configura os bancos de dados (Necessário pois temos mais de um Bind!)
     "analytics": getenv("ANALYTICS"), # Analytics usado para parcerias
     "lojas": getenv("LOJAS"), # Lojas do Hubbix em Geral
@@ -19,19 +26,20 @@ app.config["SQLALCHEMY_BINDS"] = {  # Configura os bancos de dados (Necessário 
     "manager": getenv("MANAGER"), # Hubbix Manager
     "bks": getenv("BKS"), # BK Schedular
 }
-
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = { # Configura os params do BD
     "pool_pre_ping": True, # Verifica se a conexão ainda está viva
     "pool_recycle": 1800, # Recria a conexão a cada 30 min
 }
+print("[✅] - API Configurada com Sucesso!") # Retorna sucesso na config. API
 
+print("Iniciando Banco de Dados...") # Init DB
 db.init_app(app)# Inicia o banco de dados no APP
+print("[✅] - Banco de Dados Iniciado com Sucesso!") # Success DB
 
 # Carrega os Blueprints a partir do arquivo utils/blueprints.py
+print("Cadastrando rotas... 🚀") # Init BP
 for bp in blueprints: app.register_blueprint(bp, url_prefix=blueprints[bp])
+print("Todas rotas cadastradas com sucesso ✅") # Sucess BP
 
-@app.route("/")
-def main(): return jsonify("API's running")
-
-# Modo Dev
+# Modo Desenvolvimento
 if __name__ == "__main__": app.run(debug=True, host="0.0.0.0", port=int(getenv("PORT", 9560)))
